0e870a23aa36e101f5e8b7500040b2755d87a055
Yurong Zhao <yurong.zhao@lianatech.com>
2020-05-02 22:43:08 +0300

    Part 1, Exercises 1.2: course information, step2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,40 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const exercises1 = 10
+const exercises2 = 7
+const exercises3 = 14
 const Header = (props) => (
-	<h1>
-		{props.course}
-	</h1>
+    <h1>
+        {props.course}
+    </h1>
 )
 
-const Content = (props) => (
-	<p>
-		{props.exercise} {props.score}
-	</p>
+const Part = (props) => (
+    <p>
+        {props.exercise} {props.score}
+    </p>
 )
+const Content = () => {
+    const part1 = 'Fundamentals of React'
+    const part2 = 'Using props to pass data'
+    const part3 = 'State of a component'
+
+    return (
+        <>
+        <Part exercise={part1} score={exercises1}/>
+        <Part exercise={part2} score={exercises2}/>
+        <Part exercise={part3} score={exercises3}/>
+        </>
+    )
+}
+
 const Total = (props) => (
-	<p>
-	Number of exercises {props.score}
-	</p>
+    <p>
+    Number of exercises {props.score}
+    </p>
 )
-const App = () => {
-	const course = 'Half Stack application developemnt'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
 
-	return (
-		<div>
-			<Header course={course} />
-			<Content exercise={part1} score={exercises1}/>
-			<Content exercise={part2} score={exercises2}/>
-			<Content exercise={part3} score={exercises3}/>
-			<Total score={exercises1 + exercises2 + exercises3}/>
-		</div>
-	)
+const App = () => {
+    const course = 'Half Stack application developemnt'
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content />
+            <Total score={exercises1 + exercises2 + exercises3}/>
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
