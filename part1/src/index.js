74c2836d289fb23d9c04c20aab2cf03999becf49
Yurong Zhao <yurong.zhao@lianatech.com>
2020-05-16 21:09:27 +0300

    1.7: unicafe step2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,24 +1,54 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-// 1.6: unicafe step1
+
+const History = (props) => {
+
+	if (props.allClicks === 0) {
+	  return (
+		<>
+		</>
+	  )
+	} else {
+		return (
+			<>
+			<h1>Statistics</h1>
+			<p>Good {props.good}<br/>
+			Neutral {props.neutral}<br/>
+			Bad {props.bad}<br/>
+			All {props.allClicks}<br/>
+			average {props.average}<br/>
+			positive {props.positive}%</p>
+			</>
+		  )
+	}
+}
+
+const Button = (props) => (
+	<button onClick={props.handleClick}>{props.text}</button>
+)
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(0)
+  const average = (good - bad)/allClicks
+  const positive = (good/allClicks*100)
 
+  const handleClick = (newValue, allValue, setValue) => () => {
+		setValue(newValue)
+		setAll(allValue)
+  }
 
   return (
     <div>
       <h1>Give feedback</h1>
-	  <button onClick={()=> setGood(good + 1)}>Good</button>
-	  <button onClick={()=> setNeutral(neutral + 1)}>Neutral</button>
-	  <button onClick={()=> setBad(bad + 1)}>Bad</button>
-	  <h1>Statistics</h1>
-	  <p>Good {good}</p>
-	  <p>Neutral {neutral}</p>
-	  <p>Bad {bad}</p>
+	  <Button handleClick={handleClick(good + 1, allClicks + 1, setGood)} text='Good' />
+	  <Button handleClick={handleClick(neutral + 1, allClicks + 1, setNeutral)} text='Neutral' />
+	  <Button handleClick={handleClick(bad + 1, allClicks + 1, setBad)} text='Bad' />
+
+	  <History allClicks={allClicks} good={good} neutral={neutral} bad={bad} average={average} positive={positive}/>
     </div>
   )
 }
