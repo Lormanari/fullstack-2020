19b3d35a39dab5ed500f85e0694483d258da429b
Yurong Zhao <yurong.zhao@lianatech.com>
2020-05-16 20:04:33 +0300

    1.6: unicafe step1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,65 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-const Header = (props) => (
-    <h1>
-        {props.course}
-    </h1>
-)
-
-const Content = (props) => {
-    return (
-        <>
-            <p>
-                {props.parts[0].name} {props.parts[0].exercises}
-            </p>
-            <p>
-                {props.parts[1].name} {props.parts[1].exercises}
-            </p>
-            <p>
-                {props.parts[2].name} {props.parts[2].exercises}
-            </p>
-        </>
-    )
-}
-
-const Total = (props) => {
-    const totalScore = <>{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</>
-    return (
-        <p>
-        Number of exercises {totalScore}
-        </p>
-    )
-}
-
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+// 1.6: unicafe step1
 
 const App = () => {
-    const course = {
-		name: 'Half Stack application development',
-		parts: [
-		  {
-			name: 'Fundamentals of React',
-			exercises: 10
-		  },
-		  {
-			name: 'Using props to pass data',
-			exercises: 7
-		  },
-		  {
-			name: 'State of a component',
-			exercises: 14
-		  }
-		]
-	}
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-    return (
-        <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    )
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+	  <button onClick={()=> setGood(good + 1)}>Good</button>
+	  <button onClick={()=> setNeutral(neutral + 1)}>Neutral</button>
+	  <button onClick={()=> setBad(bad + 1)}>Bad</button>
+	  <h1>Statistics</h1>
+	  <p>Good {good}</p>
+	  <p>Neutral {neutral}</p>
+	  <p>Bad {bad}</p>
+    </div>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
