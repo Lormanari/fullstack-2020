f4c9777219954eb91a53673d634f9637abe8cd68
Yurong Zhao <yurong.zhao@lianatech.com>
2020-05-19 22:44:49 +0300

    2.3*: Course contents step8

--- part2/coursecontents/src/components/Total.js
+++ part2/coursecontents/src/components/Total.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Total = ({ course }) => {
-	const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[3].exercises
+	// const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[3].exercises
+	const total = course.parts.reduce((sum, current) => sum + current.exercises, 0)
+
 	return(
-	  <p><b>Total of {sum} exercises</b></p>
+	  <p><b>Total of {total} exercises</b></p>
 	)
   }
 
