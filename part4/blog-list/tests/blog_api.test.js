a9edcb94b6ce4bbc88ba149b32a982bb679c5e90
Yurong Zhao <yurong.zhao@lianatech.com>
2020-10-07 15:51:34 +0300

    4.9*: Blog list tests, step2

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -1,28 +1,15 @@
 const supertest = require('supertest')
 const mongoose = require('mongoose')
-
+const helper = require('./test_helper')
 const app = require('../app')
 
 const api = supertest(app)
 
 const Blog = require('../models/blog')
-const initialBlogs = [
-	{
-		title: 'Go To Statement Considered Harmful',
-		author: 'Edsger W. Dijkstra',
-		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-		likes: 5
-	},
-	{
-		title: 'Canonical string reduction',
-		author: 'Edsger W. Dijkstra',
-		url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-		likes: 12
-	},
-]
+
 beforeEach(async () => {
 	await Blog.deleteMany({})
-	for (let blog of initialBlogs) {
+	for (let blog of helper.initialBlogs) {
 		let blogObject = new Blog(blog)
 		await blogObject.save()
 	}
@@ -33,6 +20,24 @@ test('blogs are returned as json', async () => {
 		.get('/api/blogs')
 		.expect(200)
 		.expect('Content-Type', /application\/json/)
+})
+
+test('all notes are returned', async () => {
+	const response = await api.get('/api/blogs')
+
+	expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+// test('a specific blog is within the returned blogs', async () => {
+// 	const response = await api.get('/api/blogs')
+
+// 	const contents = response.body.map(r => r.title)
+// 	expect(contents).toContain('Canonical string reduction')
+// })
+
+test('the unique identifier property of the blog posts is named id', async () => {
+	const blogsAtEnd = await helper.blogsInDb()
+	expect(blogsAtEnd[0].id).toBeDefined()
 })
 
 afterAll(() => {
