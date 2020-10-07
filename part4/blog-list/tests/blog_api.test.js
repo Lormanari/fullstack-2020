6c9cb34cde8b7ada5662f46bf2612564cf696a8e
Yurong Zhao <yurong.zhao@lianatech.com>
2020-10-07 16:02:13 +0300

    4.10: Blog list tests, step3

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -31,13 +31,40 @@ test('all notes are returned', async () => {
 // test('a specific blog is within the returned blogs', async () => {
 // 	const response = await api.get('/api/blogs')
 
-// 	const contents = response.body.map(r => r.title)
+// 	const titles = response.body.map(r => r.title)
 // 	expect(contents).toContain('Canonical string reduction')
 // })
 
 test('the unique identifier property of the blog posts is named id', async () => {
 	const blogsAtEnd = await helper.blogsInDb()
 	expect(blogsAtEnd[0].id).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+	const newBlog = {
+		title: 'React patterns',
+		author: 'Michael Chan',
+		url: 'https://reactpatterns.com/',
+		likes: 7,
+	}
+
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(200)
+		.expect('Content-Type', /application\/json/)
+
+	// const response = await api.get('/api/blogs')
+	// expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+	// const titles = response.body.map(r => r.title)
+	// expect(titles).toContain('React patterns')
+
+	//refactor
+	const blogsAtEnd = await helper.blogsInDb()
+	expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+	const titles = blogsAtEnd.map(n => n.title)
+	expect(titles).toContain('React patterns')
 })
 
 afterAll(() => {
